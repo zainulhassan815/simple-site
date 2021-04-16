ab2f4626ad7770fa4729d4e828d3fb0f717f5795
Zain Ul Hassan <zainulhassan4330@gmail.com>
2021-04-16 23:23:05 +0500

    Version 1.0.1

---
+++ README.md
@@ -0,0 +1,21 @@
+<p align="center">
+  <a href="" rel="noopener">
+ <img width=150px height=150px src="assets\icons\logo.jpg" alt="Project logo" style="border-radius:50%;"></a>
+</p>
+
+<h1 align="center">Basic Site Made With HTML, CSS & JS</h1>
+
+<p align="center"> This is the first site that I have made using html, css and vanilla javascript. It was quite fun building this simple site.
+    <br> 
+</p>
+
+## ⛏️ Built Using <a name = "built_using"></a>
+
+- HTML
+- CSS
+- JavaScript
+
+## ✍️ Authors <a name = "authors"></a>
+
+- [@zainulhassan](https://github.com/zainulhassan) - Idea & Initial work
+
---
+++ js/index.js
@@ -0,0 +1,104 @@
+
+window.onload = function () {
+
+    const NAVLINKS = document.querySelectorAll('.nav-link');
+    const HAMBURGER = document.querySelector('.hamburger');
+    const HAMBURGER_TEXT = document.querySelector('.hamburger i');
+    const PLAYSTORE_URL = 'https://play.google.com';
+    const PROMR_URL = 'https://www.youtube.com/c/QuickTipsbyzain';
+    const NAVBAR = document.querySelector('.navbar');
+
+    window.onresize = function () {
+        if (window.innerWidth > 500) {
+            if (NAVLINKS[0].classList.contains('active')) {
+                NAVLINKS.forEach((element) => {
+                    element.classList.remove('active');
+                });
+                HAMBURGER_TEXT.innerHTML = 'menu';
+            }
+        }
+    }
+
+    window.onscroll = function() {
+        // Check Scroll Y and increase nabar shadow
+        if (window.scrollY > 0) {
+            NAVBAR.classList.add('navbar-scroll');
+        } else {
+            NAVBAR.classList.remove('navbar-scroll');
+        }
+    }
+
+    // Handle Menu Click
+    HAMBURGER.onclick = function () {
+
+        if (NAVLINKS[0].classList.contains('active')) {
+            // Menu is visible
+            NAVLINKS.forEach((element) => {
+                element.classList.remove('active');
+            })
+            HAMBURGER_TEXT.innerHTML = 'menu';
+        } else {
+            // Menu is not visible
+            NAVLINKS.forEach((element) => {
+                element.classList.add('active');
+            })
+            HAMBURGER_TEXT.innerHTML = 'close';
+        }
+    }
+
+    // Handle Download Btn Click
+    var downloadBtn = document.querySelector('.download-btn');
+    downloadBtn.onclick = function() {
+        window.open(PLAYSTORE_URL,'_blank');
+    }
+
+    // Handle Promo Btn Click
+    var promoBtn = document.querySelector('.promo-btn');
+    promoBtn.onclick = function() {
+        window.open(PROMR_URL);
+    }
+
+    //Handle Clicks on Project Cards
+    const PROJECT_URLS = [
+        'https://youtu.be/C42NAQJG6Eg',
+        'https://community.kodular.io/t/huge-update-2-0-plyr-video-player-custom-design-qualities-captions-thumbnail-attaractive-ui/112735',
+        'https://youtu.be/8FdvPk6MWwQ',
+        'https://youtu.be/kfiozFhNhdc',
+        'https://youtu.be/mC3j2-iRZzI',
+        'https://youtu.be/LOKIpkI4BEc'
+    ];
+
+    const projectCards = document.querySelectorAll('.project');
+     
+    for(let i = 0; i < projectCards.length; i++) {
+        projectCards[i].addEventListener("click",()=>{
+            window.open(PROJECT_URLS[i]);
+        })
+    }
+
+    // Add Click function on Navbar
+    NAVLINKS.forEach((link) => {
+        link.onclick = () => {
+            if(NAVLINKS[0].classList.contains('active')) {
+                HAMBURGER.click();
+            }
+        }
+    });
+
+
+    // Add Click function to footer Icons
+    const FOOTER_LINKS = document.querySelectorAll('.footer-link');
+
+    const footerLinks = [
+        'https://www.youtube.com/channel/UCSSQg8iowZRTSP4kCypUYdw',
+        'https://web.facebook.com/groups/1105102589835321',
+        'https://t.me/zainulhassan'
+    ]
+
+    for(let i = 0; i < FOOTER_LINKS.length; i++) {
+        FOOTER_LINKS[i].onclick = () => {
+            window.open(footerLinks[i]);
+        }
+    }
+ 
+}
